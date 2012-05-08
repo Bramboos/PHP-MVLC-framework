d5de04fdc437075308029362e658405a0a3cf36a
Bram Koster <info@bram-koster.nl>
2012-05-08 15:40:36 +0200

    Environmental rules deleted

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    define("SYSTEM_PATH", "/var/www/MVLC/system");
-    define("APPLICATION_PATH", "/var/www/MVLC/application");
-
-    define("ENVIRONMENT", "development");
-    
-    require_once SYSTEM_PATH.'/core/Application.php';
-    $app = new Application();
-    $app->Run();
