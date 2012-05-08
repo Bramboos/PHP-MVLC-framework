03255b8e3f756012cfe4b22f0e4bc5fcb6e4b1ec
Bram Koster <info@bram-koster.nl>
2012-05-08 15:35:38 +0200

    indexController was wrong

--- application/controllers/indexController.php
+++ application/controllers/indexController.php
@@ -2,11 +2,8 @@
 class indexController extends baseController {
 
     function indexAction() {
-        $res = $this->db->query("SELECT people_id FROM people_ip_addresses WHERE ip = '".$this->db->real_escape_string($_SERVER["REMOTE_ADDR"])."'");
-    $this->view->banned = ($res->num_rows == 0) ? false : true;
-    $row = $res->fetch_assoc();
-    $this->view->ban_id = $row['people_id'];
-    
+        $this->layout->title = 'This is the lay-out title :D';
+            
         $this->view->render('index');
     }
 }
